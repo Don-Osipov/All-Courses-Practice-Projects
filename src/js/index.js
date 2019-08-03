479318156fb0bc72e80ec3a6f0a937b85783711d
Don Osipov <donosipov@gmail.com>
2019-08-03 16:00:34 -0400

    Added loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller
 
 import Search from './models/Search';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 
 /*
@@ -25,11 +25,13 @@ const controlSearch = async () => {
         // 3. Prepare UI for results (search spinner);
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4. Search for recipes
         await state.search.getResults();
 
         // 5. Render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 };
