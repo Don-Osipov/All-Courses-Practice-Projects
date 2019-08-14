143d84d087f67cc72900b10354af043b878770b1
Don Osipov <donosipov@gmail.com>
2019-08-13 22:24:32 -0400

    THIS IS NOW A REPO FOR ALL COURSE PROJECTS
    Added react starter project

---
+++ React Starter Project__udemy--react complete guide/src/components/Persons/Person/Person.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Person.module.css';
+
+const person = (props) => {
+    // const rand = Math.random();
+    // if (rand > .7) {
+    //     throw new Error('Something went wrong')
+    // }
+    return (
+        <div className={styles.Person}>
+            <p onClick={props.click}>
+                I'm a {props.name} and I am {props.age} years old
+            </p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    );
+};
+
+export default person;
